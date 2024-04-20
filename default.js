05c78ea19871415f33077f1e4436e7e973ed431a
Swarajkr7 <swaraj853@gmail.com>
2024-04-20 12:02:53 +0530

    duplicate error resolved

--- default.js
+++ default.js
@@ -1,15 +1,26 @@
-
-import { products } from "./constants/data.js"
+import { products } from "./constants/data.js";
 import Product from "./model/product_schema.js";
 
+// Function to insert data while handling duplicates
 const DefaultData = async() => {
     try {
-           await Product.insertMany(products);
-
-            console.log("Data imported Successfully");
+        // Define options for insertMany
+        const options = { ordered: false };
+        
+        // Find existing product IDs in the collection
+        const existingProductIds = new Set(await Product.find().distinct('id'));
+        
+        // Filter out products with duplicate IDs
+        const uniqueProducts = products.filter(product => !existingProductIds.has(product.id));
+        
+        // Insert filtered data
+        await Product.insertMany(uniqueProducts, options);
+        
+        console.log("Data imported successfully");
     } catch (error) {
-        console.log("Error while inserting default data ", error.message);
+        console.log("Error while inserting default data", error.message);
     }
 }
 
-export default  DefaultData;
+// Export the DefaultData function as default
+export default DefaultData;
